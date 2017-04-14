eb562e25d73eaf0ea4bf7ea754caff81aa6de378
Navrotskyj <navrotskyj@gmail.com>
2017-04-14 17:53:41 +0300

    bug fix

--- src/modules/accounts/accountModel.js
+++ src/modules/accounts/accountModel.js
@@ -79,7 +79,7 @@ define(['app', 'scripts/webitel/utils', 'modules/acd/acdModel'], function (app, 
 
             return "'{originate_timeout=" + time +
                 ",agent_name='" + name + "'" +
-                ",api_on_answer=\\\'uuid_setvar ${uuid} agent_name " + name  + "\\\'"+
+                // ",api_on_answer=\\\'uuid_setvar ${uuid} agent_name " + name  + "\\\'"+
                 ",presence_id=" + id + "@" + domain + "}{webitel_call_uuid=${create_uuid()},sip_invite_domain=" + domain +
                 "}${sofia_contact(*/" + id + "@" + domain +
                 ")},${verto_contact(" + id + "@" + domain + ")}'";
