618252ce1505d9d6349d3c137d37b7ee70e8c247
matvij <m.plikhivskyi@webitel.com>
2017-09-19 13:02:04 +0300

    cdr charts

--- src/modules/cdr/cdrElastic.js
+++ src/modules/cdr/cdrElastic.js
@@ -72,10 +72,18 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                 if($scope.panelStatistic){
                     $scope.getInboundStats();
                     $scope.getDirectionStats();
+                    $scope.getAvgStats();
                 }
             }, true);
 
-
+            $scope.$watch("domain", function (newVal) {
+                $scope.panelStatistic=false;
+                // if(){
+                //     $scope.getInboundStats();
+                //     $scope.getDirectionStats();
+                //     $scope.getAvgStats();
+                // }
+            }, true);
 
             $scope.mapColumns = CdrModel.mapColumn();
             $scope.columns = CdrModel.availableColumns();
@@ -537,52 +545,75 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                 if(value){
                     $scope.getInboundStats();
                     $scope.getDirectionStats();
+                    $scope.getAvgStats();
                 }
             };
 
             $scope.getInboundStats = function () {
                 $scope.statRequests.inbound.filter[1].range['variables.start_stamp'].gte = $scope.startDate.getTime();
                 $scope.statRequests.inbound.filter[1].range['variables.start_stamp'].lte = $scope.endDate.getTime();
+                $scope.statRequests.inbound.domain = $scope.domain;
                 CdrModel.getStatistic($scope.domain, $scope.statRequests.inbound, function(err, res){
                     if (err)
                         return notifi.error(err);
                     $scope.inbound_total = res.hits.total;
-                    $scope.inbound_answered = res.aggregations.Answered.value;
-                    $scope.inbound_abandoned = res.aggregations.Abandoned.value;
+                    $scope.inbound_answered = res.aggregations && res.aggregations.Answered.value;
+                    $scope.inbound_abandoned = res.aggregations && res.aggregations.Abandoned.value;
                 });
             };
 
             $scope.getAvgStats = function () {
                 $scope.statRequests.avg.filter[0].range['variables.start_stamp'].gte = $scope.startDate.getTime();
                 $scope.statRequests.avg.filter[0].range['variables.start_stamp'].lte = $scope.endDate.getTime();
+                $scope.statRequests.avg.domain = $scope.domain;
                 CdrModel.getStatistic($scope.domain, $scope.statRequests.avg, function(err, res){
                     if (err)
                         return notifi.error(err);
+                    if(!!res.aggregations)
+                        res.aggregations.avgg.buckets.Bridged.avggg.buckets.forEach(function (item) {
+                            $scope.avgConnectedCallMetr.data[0].values.push({
+                                label: item.key,
+                                value: item.bbd.value
+                            });
+                            $scope.avgConnectedCallMetr.data[1].values.push({
+                                label: item.key,
+                                value: item.ccd.value
+                            });
+                            $scope.avgConnectedCallMetr.data[2].values.push({
+                                label: item.key,
+                                value: item.cd.value
+                            })
+                        });
                 });
             };
 
             $scope.getDirectionStats = function () {
                 $scope.statRequests.direction.filter[0].range['variables.start_stamp'].gte = $scope.startDate.getTime();
                 $scope.statRequests.direction.filter[0].range['variables.start_stamp'].lte = $scope.endDate.getTime();
+                $scope.statRequests.direction.domain = $scope.domain;
                 var data = [];
                 CdrModel.getStatistic($scope.domain, $scope.statRequests.direction, function(err, res){
                     if (err)
                         return notifi.error(err);
-                    res.aggregations.directions.buckets.forEach(function (item) {
-                       data.push({
-                           key: item.key,
-                           y: item.doc_count
-                       });
-                    });
-                    $scope.callDirection.data = data;
-                    data = [];
-                    res.aggregations.hangup_causes.buckets.forEach(function (item) {
-                        data.push({
-                            label: item.key,
-                            value: item.doc_count
+                    if(res.aggregations && res.aggregations.directions){
+                        res.aggregations.directions.buckets.forEach(function (item) {
+                            data.push({
+                                key: item.key,
+                                y: item.doc_count
+                            });
                         });
-                    });
-                    $scope.causeByAttemptChart.data[0].values = data;
+                        $scope.callDirection.data = data;
+                    }
+                    if(res.aggregations && res.aggregations.hangup_causes){
+                        data = [];
+                        res.aggregations.hangup_causes.buckets.forEach(function (item) {
+                            data.push({
+                                label: item.key,
+                                value: item.doc_count
+                            });
+                        });
+                        $scope.causeByAttemptChart.data[0].values = data;
+                    }
                 });
             }
 
@@ -631,7 +662,7 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                 },
                 avg:{
                     "aggs": {
-                        "2": {
+                        "avgg": {
                             "filters": {
                                 "filters": {
                                     "Bridged": {
@@ -643,7 +674,7 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                                 }
                             },
                             "aggs": {
-                                "3": {
+                                "avggg": {
                                     "terms": {
                                         "field": "User ID",
                                         "size": 10,
@@ -652,17 +683,17 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                                         }
                                     },
                                     "aggs": {
-                                        "5": {
+                                        "cd": {
                                             "avg": {
                                                 "field": "Call duration"
                                             }
                                         },
-                                        "6": {
+                                        "bbd": {
                                             "avg": {
                                                 "field": "Before Bridge Delay"
                                             }
                                         },
-                                        "7": {
+                                        "ccd": {
                                             "avg": {
                                                 "field": "Connected call duration"
                                             }
@@ -790,132 +821,48 @@ define(['app', 'moment', 'jsZIP', 'async', 'modules/cdr/cdrModel', 'modules/cdr/
                     }
                 },
                 data: [
-                    // {
-                    //     "key": "Bridged: Avg answer delay, s",
-                    //     "color": "#d62728",
-                    //     "values": [
-                    //         {
-                    //             "label" : "Group A" ,
-                    //             "value" : 1.8746444827653
-                    //         } ,
-                    //         {
-                    //             "label" : "Group B" ,
-                    //             "value" : 5.0961543492239
-                    //         } ,
-                    //         {
-                    //             "label" : "Group C" ,
-                    //             "value" : 0.57072943117674
-                    //         } ,
-                    //         {
-                    //             "label" : "Group D" ,
-                    //             "value" : 2.4174010336624
-                    //         } ,
-                    //         {
-                    //             "label" : "Group E" ,
-                    //             "value" : 0.72009071426284
-                    //         } ,
-                    //         {
-                    //             "label" : "Group F" ,
-                    //             "value" : 0.77154485523777
-                    //         } ,
-                    //         {
-                    //             "label" : "Group G" ,
-                    //             "value" : 0.90152097798131
-                    //         } ,
-                    //         {
-                    //             "label" : "Group H" ,
-                    //             "value" : 0.91445417330854
-                    //         } ,
-                    //         {
-                    //             "label" : "Group I" ,
-                    //             "value" : 0.055746319141851
-                    //         }
-                    //     ]
-                    // },
-                    // {
-                    //     "key": "Bridged: Avg talk time, s",
-                    //     "color": "#1f77b4",
-                    //     "values": [
-                    //         {
-                    //             "label" : "Group A" ,
-                    //             "value" : 25.307646510375
-                    //         } ,
-                    //         {
-                    //             "label" : "Group B" ,
-                    //             "value" : 16.756779544553
-                    //         } ,
-                    //         {
-                    //             "label" : "Group C" ,
-                    //             "value" : 18.451534877007
-                    //         } ,
-                    //         {
-                    //             "label" : "Group D" ,
-                    //             "value" : 8.6142352811805
-                    //         } ,
-                    //         {
-                    //             "label" : "Group E" ,
-                    //             "value" : 7.8082472075876
-                    //         } ,
-                    //         {
-                    //             "label" : "Group F" ,
-                    //             "value" : 5.259101026956
-                    //         } ,
-                    //         {
-                    //             "label" : "Group G" ,
-                    //             "value" : 0.30947953487127
-                    //         } ,
-                    //         {
-                    //             "label" : "Group H" ,
-                    //             "value" : 0
-                    //         } ,
-                    //         {
-                    //             "label" : "Group I" ,
-                    //             "value" : 0
-                    //         }
-                    //     ]
-                    // },
-                    // {
-                    //     "key": "Bridged: Avg call duration, s",
-                    //     "color": "#ff7f0e",
-                    //     "values": [
-                    //         {
-                    //             "label" : "Group A" ,
-                    //             "value" : 25.307646510375
-                    //         } ,
-                    //         {
-                    //             "label" : "Group B" ,
-                    //             "value" : 16.756779544553
-                    //         } ,
-                    //         {
-                    //             "label" : "Group C" ,
-                    //             "value" : 18.451534877007
-                    //         } ,
-                    //         {
-                    //             "label" : "Group D" ,
-                    //             "value" : 8.6142352811805
-                    //         } ,
-                    //         {
-                    //             "label" : "Group E" ,
-                    //             "value" : 7.8082472075876
-                    //         } ,
-                    //         {
-                    //             "label" : "Group F" ,
-                    //             "value" : 5.259101026956
-                    //         } ,
-                    //         {
-                    //             "label" : "Group G" ,
-                    //             "value" : 0.30947953487127
-                    //         } ,
-                    //         {
-                    //             "label" : "Group H" ,
-                    //             "value" : 0
-                    //         } ,
-                    //         {
-                    //             "label" : "Group I" ,
-                    //             "value" : 0
-                    //         }
-                    //     ]
-                    // }
+                    {
+                        "key": "Bridged: Avg answer delay, s",
+                        "color": "#44d680",
+                        "values": [
+                            // {
+                            //     "label" : "Group A" ,
+                            //     "value" : 1.8746444827653
+                            // },
+                            // {
+                            //     "label" : "Group B" ,
+                            //     "value" : 5.0961543492239
+                            // }
+                        ]
+                    },
+                    {
+                        "key": "Bridged: Avg talk time, s",
+                        "color": "#1f77b4",
+                        "values": [
+                            // {
+                            //     "label" : "Group A" ,
+                            //     "value" : 25.307646510375
+                            // },
+                            // {
+                            //     "label" : "Group B" ,
+                            //     "value" : 16.756779544553
+                            // }
+                        ]
+                    },
+                    {
+                        "key": "Bridged: Avg call duration, s",
+                        "color": "#ff7f0e",
+                        "values": [
+                            // {
+                            //     "label" : "Group A" ,
+                            //     "value" : 25.307646510375
+                            // },
+                            // {
+                            //     "label" : "Group B" ,
+                            //     "value" : 16.756779544553
+                            // }
+                        ]
+                    }
                 ]
             };
 
