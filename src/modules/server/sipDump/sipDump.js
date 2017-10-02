fcf8d8a1edecf7243620f98f973cd208dd3d9444
matvij <m.plikhivskyi@webitel.com>
2017-10-02 13:32:39 +0300

    sipDump modal

--- src/modules/server/sipDump/sipDump.js
+++ src/modules/server/sipDump/sipDump.js
@@ -5,8 +5,43 @@
 
 define(['app', 'scripts/webitel/utils', 'modules/server/sipDump/sipDumpModel'], function (app, utils) {
 
-    app.controller('ServerSipDumpCtrl', ['$scope', 'webitel', '$rootScope', 'notifi', '$timeout', 'ServerSipDumpModel', '$confirm',
-        function ($scope, webitel, $rootScope, notifi, $timeout, ServerSettingsModel, $confirm) {
+    app.controller('ServerSipDumpCtrl', ['$scope', '$modal', 'webitel', '$rootScope', 'notifi', '$timeout', 'ServerSipDumpModel', '$confirm',
+        function ($scope, $modal, webitel, $rootScope, notifi, $timeout, ServerSettingsModel, $confirm) {
+
+            $scope.add = function(){
+                var modalInstance = $modal.open({
+                    animation: true,
+                    backdrop: false,
+                    templateUrl: '/modules/server/sipDump/sipDumpModal.html',
+                    resolve: {
+                        domainName: function () {
+                            return $scope.domain;
+                        }
+                    },
+                    controller: function ($modalInstance, $scope, domainName) {
+                        var self = $scope;
+
+                        self.duration = 0;
+                        self.filtr = '';
+
+                        self.ok = function () {
+                            $modalInstance.close({
+                                duration: self.duration,
+                                filter: self.filtr
+                            });
+                        };
+
+                        self.cancel = function () {
+                            $modalInstance.dismiss('cancel');
+                        };
+                    }
+                });
+
+                modalInstance.result.then(function (option) {
+                    debugger;
+                });
+            }
+
 
         }
     ]);
