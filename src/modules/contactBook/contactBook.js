82e86adab247f429ad8d3fed879cd50e4558d02a
matvij <m.plikhivskyi@webitel.com>
2017-10-18 14:46:17 +0300

    WTEL-543

--- src/modules/contactBook/contactBook.js
+++
@@ -1,8 +0,0 @@
-define(['app', 'ngTable'], function (app) {
-
-    app.controller('GatewaysCtrl', ['$scope', 'NgTableParams', 'webitel', '$rootScope', 'notifi',
-        function ($scope, NgTableParams, webitel, $rootScope, notifi) {
-
-
-    }])
-});
