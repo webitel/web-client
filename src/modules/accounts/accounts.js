efd544e51fcec591cac291b4da5023ea573d0946
matvij <m.plikhivskyi@webitel.com>
2018-04-26 11:19:49 +0300

    devices WTEL-664

--- src/modules/accounts/accounts.js
+++ src/modules/accounts/accounts.js
@@ -283,7 +283,7 @@ define(['app', 'scripts/webitel/utils',  'async', 'modules/accounts/accountModel
 
         function genString(num) {
             var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_+";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
             for( var i=0; i < num; i++ )
                 text += possible.charAt(Math.floor(Math.random() * possible.length));
